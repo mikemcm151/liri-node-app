994b789008d46bb034572d3600b4521200bc956f
mikemcm151 <mikemcm151@gmail.comgit config --global user.name mikemcm151git config --global user.email mikemcm151@gmail.com>
2017-12-07 14:15:30 -0600

    twitter key

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'bgFxrma4rhtbsNOEVuHaxUx1l',
+  consumer_secret: 'EOMB1qIRjzHNMUL6UShUjs2PLCVL3sw6hKuJjvSmf33Jq4QHFF',
+  access_token_key: '	938655171592192001-FVuFIVWx0ZBuk58Shq8AqNLfMJrZO9z',
+  access_token_secret: 'Tx8Ng7LIgMlwfcWiQZiWWft7JBgUdPB0NGv87704N15NC',
+}
+
+module.exports = twitterKeys;
